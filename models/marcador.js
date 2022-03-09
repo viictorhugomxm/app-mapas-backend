7d6738bab0f68977a742f0c2222cc04b6e1f07e8
Victor Hugo Segundo Aguilar <viictorhugo.dev@gmail.com>
2022-03-09 03:43:53 -0600

    Backend inicial para app de mapas listo

---
+++ models/marcador.js
@@ -0,0 +1,9 @@
+class Marcador {
+  constructor(id, lng,lat) {
+      this.id = id;
+      this.lng = lng;
+      this.lat = lat
+  }
+}
+
+module.exports = Marcador;
---
+++ models/marcadores.js
@@ -0,0 +1,10 @@
+class Marcadores {
+  constructor() {
+    this.activos = {}
+  }
+
+  agregarMarcador() {
+    this.activos[marcador.id] = marcador
+    return marcador
+  }
+}
